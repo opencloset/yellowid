29c41dee97baced76b106b9868ce9ea0c97d6abb
Hyungsuk Hong <aanoaa@gmail.com>
2016-02-26 13:43:17 +0900

    Add Root controller and some routes

--- lib/OpenCloset/YellowID.pm
+++ lib/OpenCloset/YellowID.pm
@@ -1,18 +1,47 @@
 package OpenCloset::YellowID;
 use Mojo::Base 'Mojolicious';
 
-# This method will run once at server start
+=head1 METHODS
+
+=head2 startup
+
+This method will run once at server start
+
+=cut
+
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    $self->plugin('Config');
 
-  # Router
-  my $r = $self->routes;
+    $self->secrets( $self->config->{secrets} );
+    $self->sessions->cookie_name('opencloset');
+    $self->sessions->default_expiration(86400);
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    $self->_public_routes;
+    $self->_private_routes;
+}
+
+=head2 _public_routes
+
+=cut
+
+sub _public_routes {
+    my $self = shift;
+    my $r    = $self->routes;
+
+    $r->get('/keyboard')->to('root#keyboard')->name('keyboard');
+    $r->post('/message')->to('root#create_message')->name('message.create');
+    $r->post('/friend')->to('root#create_friend')->name('friend.create');
+    $r->delete('/friend')->to('root#delete_friend')->name('friend.delete');
+    $r->delete('/chat_room/:user_key')->to('root#quit_room')->name('room.quit');
+}
+
+=head2 _private_routes
+
+=cut
+
+sub _private_routes {
 }
 
 1;
---
+++ lib/OpenCloset/YellowID/Controller/Root.pm
@@ -0,0 +1,88 @@
+package OpenCloset::YellowID::Controller::Root;
+use Mojo::Base 'Mojolicious::Controller';
+
+=head1 METHODS
+
+=head2 keyboard
+
+    # keyboard
+    GET /keyboard
+
+=cut
+
+sub keyboard {
+    my $self = shift;
+
+    $self->render( json => { type => 'buttons', buttons => ['선택1', '선택2', '선택3'] } );
+}
+
+=head2 create_message
+
+    # message.create
+    POST /message
+
+=cut
+
+sub create_message {
+    my $self = shift;
+
+    my $user_key = $self->param('user_key') || '';
+    my $type     = $self->param('type')     || '';
+    my $content  = $self->param('content')  || '';
+
+    $self->log->debug($user_key);
+    $self->log->debug($type);
+    $self->log->debug($content);
+
+    $self->render( json => { text => '귀하의 차량이 성공적으로 등록되었습니다. 축하합니다!' } );
+}
+
+=head2 create_friend
+
+    # friend.create
+    POST /friend
+
+=cut
+
+sub create_friend {
+    my $self = shift;
+
+    my $user_key = $self->param('user_key') || '';
+    $self->log->debug($user_key);
+
+    $self->render( json => {} );
+}
+
+=head2 delete_friend
+
+    # friend.delete
+    DELETE /friend
+
+=cut
+
+sub delete_friend {
+    my $self = shift;
+
+    my $user_key = $self->param('user_key') || '';
+    $self->log->debug($user_key);
+
+    $self->render( json => {} );
+}
+
+=head2 quit_room
+
+    # room.quit
+    DELETE /chat_room/:user_key
+
+=cut
+
+sub quit_room {
+    my $self     = shift;
+    my $user_key = $self->param('user_key');
+
+    $self->log->debug($user_key);
+
+    $self->render( json => {} );
+}
+
+1;
